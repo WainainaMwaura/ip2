e3067a51dbb93ac1dd172283157db2258cd3c69e
WainainaMwaura <john.mwaura@student.moringaschool.com>
2021-06-23 02:15:18 +0300

    creating day of the week variables

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var akanMale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var akanFemale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
