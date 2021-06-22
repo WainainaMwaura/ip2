703c5e44a051983a7f54bc35fbb3ea7a9b350dc5
WainainaMwaura <john.mwaura@student.moringaschool.com>
2021-06-23 02:20:31 +0300

    feching values from html form

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var akanMale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var akanFemale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+function results() {
+    var century =parseInt (document.getElementById("century").value);
+    var month =parseInt(document.getElementById("month").value);
+    var year =parseInt(document.getElementById("year").value);
+    var day=parseInt(document.getElementById("monthday").value);
+    var date=new Date(year + "/" + month + "/" + day);
+    var gender= print();
+    var day2=date.getDay();
+    var akan; }
